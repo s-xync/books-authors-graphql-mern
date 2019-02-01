281ddedd7b6a2cf20f58e9639d6b1bd9770138de
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-02 00:42:19 +0530

    adds basic books schema

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 const PORT = process.env.PORT || 4000;
 
