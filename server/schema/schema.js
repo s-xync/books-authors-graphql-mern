bcf7f197e64cb9a92ed5b311be3caddf12777158
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-03 21:59:02 +0530

    more queries in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
@@ -45,7 +46,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
@@ -64,6 +71,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env.development" });
 
 const app = express();
+
+// connect with mlab
+const MONGODB_URI = "mongodb://127.0.0.1:27017/gql";
+// const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true });
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
 
 app.use(
   "/graphql",
