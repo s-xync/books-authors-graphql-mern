cd24e9accc075bfdee8c61ffe5fdce503e121a24
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-06 00:04:05 +0530

    basic query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  url: "/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Hi!</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Hi!</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
