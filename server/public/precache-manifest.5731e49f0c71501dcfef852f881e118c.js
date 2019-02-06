18894cfc031f277fdf2d6313295bf14e3236e9bd
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-06 11:31:25 +0530

    small changes to make the project deployable

---
+++ server/public/precache-manifest.5731e49f0c71501dcfef852f881e118c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1fea26356079b1dbe1d",
+    "url": "/static/js/main.b1fea263.chunk.js"
+  },
+  {
+    "revision": "dba2f7b82b3b3e989c92",
+    "url": "/static/js/1.dba2f7b8.chunk.js"
+  },
+  {
+    "revision": "b1fea26356079b1dbe1d",
+    "url": "/static/css/main.427f596c.chunk.css"
+  },
+  {
+    "revision": "af0624158397ed6a1b1628f42a55fc52",
+    "url": "/index.html"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const path = require("path");
+
 require("dotenv").config({ path: ".env.development" });
 const cors = require("cors");
 
@@ -10,9 +12,9 @@ const app = express();
 // cors policies
 app.use(cors());
 
-// connect with mlab
+// connect with mongodb
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/gql";
 const MONGODB_URI = "mongodb://127.0.0.1:27017/gql";
-// const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true });
 mongoose.connection.once("open", () => {
@@ -26,6 +28,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 4000;
 
